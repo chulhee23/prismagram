d27c3b41f1bf7d53dc9e103eee0b60818c6d86af
chulhee23 <chulhee2351@gmail.com>
2020-03-05 10:25:11 +0900

    #3.6 passport jwt pt3

--- src/server.js
+++ src/server.js
@@ -1,20 +1,21 @@
-import dotenv from "dotenv";
-import path from "path";
-dotenv.config({path: path.resolve(__dirname, ".env")})
-
+import "./env";
 import { GraphQLServer } from "graphql-yoga";
 import logger from "morgan";
-import passport from "passport"
 import schema from "./schema";
-import "./passport"
+import { authenticateJwt } from "./passport";
 
 
 const PORT = process.env.PORT || 4000;
 
-const server = new GraphQLServer({ schema });
+// context는 resolver 사이에서 정보 공유할 때 사용
+const server = new GraphQLServer({ schema, 
+  context: ({request}) => ({ request})
+  // passport의 req와 여기의 req는 다르다. 이 중에 passport의 req 가 들어있을 것.
+});
+
 
 server.express.use(logger("dev"));
-server.express.use("/api", passport.authenticate("jwt"));
+server.express.use(authenticateJwt);
 
 
 server.start({ port: PORT }, () =>
