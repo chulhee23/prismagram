463d01247e8a83c10eb6eb887aeab26e97978d76
chulhee23 <chulhee2351@gmail.com>
2020-03-05 16:57:25 +0900

    #3.7 toggleLike Post

--- src/passport.js
+++ src/passport.js
@@ -1,35 +1,32 @@
-import "./env"
 import passport from "passport";
-import {Strategy, ExtractJwt} from "passport-jwt";
+import { Strategy, ExtractJwt } from "passport-jwt";
+import { prisma } from "../generated/prisma-client";
 
 const jwtOptions = {
-  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(), // authorization header 에서 jwt 찾는 역할
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
   secretOrKey: process.env.JWT_SECRET
-}
+};
 
-const verifyUser  = async (payload, done) => {
+const verifyUser = async (payload, done) => {
   try {
-    const user = await prisma.user({id: payload.id})
+    const user = await prisma.user({ id: payload.id });
     if (user !== null) {
       return done(null, user);
     } else {
       return done(null, false);
-
     }
-
-  } catch(err) {
-    return done(err, false)
+  } catch (error) {
+    return done(error, false);
   }
-}
+};
 
-export const authenticateJwt = (req, res, next) => {
-  passport.authenticate("jwt", {sessions: false}, (error, user) => {
+export const authenticateJwt = (req, res, next) =>
+  passport.authenticate("jwt", { sessions: false }, (error, user) => {
     if (user) {
       req.user = user;
     }
     next();
   })(req, res, next);
 
-}
-
-passport.use(new Strategy(jwtOptions, verifyUser))
+passport.use(new Strategy(jwtOptions, verifyUser));
+passport.initialize();
--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 4000;
 
 // context는 resolver 사이에서 정보 공유할 때 사용
 const server = new GraphQLServer({ schema, 
-  context: ({request}) => ({ request})
+  context: ({ request }) => ({ request })
   // passport의 req와 여기의 req는 다르다. 이 중에 passport의 req 가 들어있을 것.
 });
 
